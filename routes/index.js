a4f039f00b41d65de8753a5cb71abd5103f038cd
bastian bastias <bastian.bastias@gmail.com>
2022-09-28 12:52:06 -0400

    services, contacpdf

--- routes/index.js
+++ routes/index.js
@@ -10,41 +10,33 @@ var fs = require('fs');
 const viewsPathPrefix = "../views";
 
 router.get("/", function (req, res) {
-    //res.sendFile(path.join(__dirname, viewsPathPrefix + "/home.html"));
-    res.render('home', { title: 'Home'});
+    res.render('home', { title: 'Home' , menu: 'home'});
 });
 
 router.use("/about_me", function (req, res) {
-    console.log("about me __dirname: "+__dirname);
-    //res.sendFile(path.join(__dirname, viewsPathPrefix + "/about_me.html"));
-    res.render('about_me', { title: 'About Me-'});
+    console.log("about me __dirname: " + __dirname);
+    res.render('about_me', { title: 'About Me', menu: 'about' });
 });
 
 router.get("/download_resume", function (req, res) {
-    //res.sendFile(path.join(__dirname + "pdfs/Bastian_Bastias_2022_resume_v01.pdf"));    
-    //res.download("./public/resume/Bastian_Bastias_2022_resume_v01.pdf");
-
     var file = "./public/resume/Bastian_Bastias_2022_resume_v01.pdf";
-
     var filename = path.basename(file);
     var mimetype = mime.lookup(file);
-  
     res.setHeader('Content-disposition', 'attachment; filename=' + filename);
     res.setHeader('Content-type', mimetype);
     res.download(file);
-  
 });
 
 router.use("/projects", function (req, res) {
-    res.sendFile(path.join(__dirname, viewsPathPrefix + "/projects.html"));
+    res.render('projects', { title: 'Projects', menu: 'projects' });
 });
 
 router.use("/services", function (req, res) {
-    res.sendFile(path.join(__dirname, viewsPathPrefix , "/services.html"));
+    res.render('services', { title: 'Services', menu: 'services' });
 });
 
 router.use("/contact_me", function (req, res) {
-    res.sendFile(path.join(__dirname, viewsPathPrefix + "/contact.html"));
+    res.render('contact', { title: 'Contact', menu: 'contact' });
 });
 
 module.exports = router;
