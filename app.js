56aedfa8f379f561acb3310c1366d6cef4de7c75
bastian bastias <bastian.bastias@gmail.com>
2022-09-28 11:58:08 -0400

    add ejs templates

---
+++ app.js
@@ -0,0 +1,26 @@
+// installed 3rd party packages
+const exp = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+//const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+//const usersRouter = require('./routes/users');
+
+const app = exp();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // express  -e
+
+//app.use(logger('dev'));
+app.use(exp.json());
+app.use(exp.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(exp.static(path.join(__dirname, 'public')));
+app.use(exp.static(path.join(__dirname, 'node_modules')));
+
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const path = require("path")
+/*app.use(bodyParser.urlencoded({ extended: true }));
+app.use(exp.static('public'))*/
+
+const viewsPathPrefix = "../views";
+
+router.get("/", function (req, res) {
+    //res.sendFile(path.join(__dirname, viewsPathPrefix + "/home.html"));
+    res.render('home', { title: 'Home'});
+});
+
+router.use("/about_me", function (req, res) {
+    res.sendFile(path.join(__dirname, viewsPathPrefix + "/about_me.html"));
+});
+
+router.use("/projects", function (req, res) {
+    res.sendFile(path.join(__dirname, viewsPathPrefix + "/projects.html"));
+});
+
+router.use("/services", function (req, res) {
+    res.sendFile(path.join(__dirname, viewsPathPrefix , "/services.html"));
+});
+
+router.use("/contact_me", function (req, res) {
+    res.sendFile(path.join(__dirname, viewsPathPrefix + "/contact.html"));
+});
+
+module.exports = router;
