7806154716ec59971820e5c083ce23e731d803c2
bastian bastias <bastian.bastias@gmail.com>
2022-10-14 20:40:18 -0400

    delete contact logic

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -2,10 +2,7 @@
 (function () {
 
     function Start() {
-        console.log("App Started...");
-
         let deleteButtons = document.querySelectorAll('.btn-danger');
-
         for (button of deleteButtons) {
             button.addEventListener('click', (event) => {
                 if (!confirm("Are you sure to?\nDo you really want to delete this contact?")) {
